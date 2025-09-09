3a12ac4759b20d319d7f65d568c9a477ef9c4054
xvvas <miguelvg097@gmail.com>
2025-09-09 07:48:56 -0500

    feat: initialize React project with Vite, add essential dependencies and configurations
    - Created package.json with scripts for development, build, linting, and preview.
    - Added Vite configuration for React with path aliases.
    - Included global styles in index.css and component styles in App.css.
    - Set up main entry point in main.jsx and basic App component in App.jsx.
    - Added SVG assets for Vite and React logos.

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@ap': fileURLToPath(new URL('./src/assets/png', import.meta.url)),
+      '@ct': fileURLToPath(new URL('./src/components/templates', import.meta.url)),
+      '@cg': fileURLToPath(new URL('./src/components/global', import.meta.url)),
+      '@ce': fileURLToPath(new URL('./src/components/pages', import.meta.url)),
+      '@cu': fileURLToPath(new URL('./src/components/util', import.meta.url)),
+    },
+  },
+  extensions: ['.js', '.jsx', '.ts', '.tsx']
+});
